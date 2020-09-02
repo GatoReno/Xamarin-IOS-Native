1ce78c2ac18c585e7dc5aeb2d89233660e656520
Gatoreno <erp.reno@hotmail.com>
2020-09-02 14:35:15 -0400

    add ios sample

---
+++ IOSFirstApp/ViewController.cs
@@ -0,0 +1,47 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace IOSFirstApp
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Console.WriteLine("ViewDidLoad Launched 📡");
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            btn1.TouchUpInside += (object sender, EventArgs e) => {
+                Console.WriteLine("CLICKED YEAH!!!!! 💀");
+                textView.Text = textField.Text;
+            };
+
+            btn1.TouchDragExit += (object sender, EventArgs e) => {
+                Console.WriteLine("TouchDragExit YEAH!!!!! 💀");
+               
+                textField.Text = "";
+            };
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            Console.WriteLine("ViewWillAppear Launched 📡");
+        }
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            Console.WriteLine("ViewWillDisappear Launched 📡");
+        }
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ IOSFirstApp/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace IOSFirstApp
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Lbl1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView textView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btn1 != null) {
+                btn1.Dispose ();
+                btn1 = null;
+            }
+
+            if (Lbl1 != null) {
+                Lbl1.Dispose ();
+                Lbl1 = null;
+            }
+
+            if (textField != null) {
+                textField.Dispose ();
+                textField = null;
+            }
+
+            if (textView != null) {
+                textView.Dispose ();
+                textView = null;
+            }
+        }
+    }
+}
